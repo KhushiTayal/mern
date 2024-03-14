1edc7ba7a1bd520c79e0d4acc894d1271c2ac20e
Khushi Tayal <khushitayal55@gmail.com>
2024-03-14 14:07:29 +0530

    deployment changes

--- backend/app.ts
+++ backend/app.ts
@@ -11,6 +11,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+// Your code
+
+
 // Connect to MongoDB
 connectDB();
 
